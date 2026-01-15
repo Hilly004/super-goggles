6260587e8201b7e0b7af28fdb582630c5e32fde0
Hilly004 <finleyghill04@gmail.com>
2026-01-15 12:55:04 +0000

    Commit.3

--- Orbits.py
+++ Orbits.py
@@ -4,6 +4,7 @@ import Positions as pos
 import Data_Extraction as datex
 import Planetary_Data as pdat
 import RK4_Method as rk
+from mpl_toolkits import mplot3d
 
 fol = '/Users/finleyhill/Documents/University/Level 3/CP/Planetary Data/'
 times = np.arange(0,4385,1)
@@ -12,16 +13,22 @@ posAst = np.zeros((len(times),3))
 #print(pos.pos('2024YR4',0))
 r = np.array(pos.vec('2024YR4',0)[1:6])
 
-for i,t in enumerate(times):
-    posAst[i] = r[:3]
-    rk.rk4(r,t,dt)
+#for i,t in enumerate(times):
+    #posAst[i] = r[:3]
+    #rk.rk4(r,t,dt)
 
-plt.figure().add_subplot(111,projection = '3d')
+ax = plt.axes(projection='3d')
 plt.subplot().set_aspect('equal')
-plt.xlim((-1e9,1e9))
-plt.ylim((-1e9,1e9))
+ax.set_xlim3d(-1e9,1e9)
+ax.set_ylim3d(-1e9,1e9)
+ax.set_zlim3d(-1e9,1e9)
+#ax.set_xticklabels((-1,-0.5,0,0.5,1))
+#ax.set_yticklabels((-1,-0.5,0,0.5,1))
+#ax.set_zticklabels((-1,-0.5,0,0.5,1))
 for name in pdat.names:
-    plt.plot(datex.out(fol+name+'.txt')[1],datex.out(fol+name+'.txt')[2],datex.out(fol+name+'.txt')[3], label = name)
+    ax.plot(datex.out(fol+name+'.txt')[1],datex.out(fol+name+'.txt')[2],datex.out(fol+name+'.txt')[3], label = name)
+    ax.plot(datex.out(fol+name+'.txt')[1][-1],datex.out(fol+name+'.txt')[2][-1],datex.out(fol+name+'.txt')[3][-1],marker = 'x')
     #plt.legend()
-plt.plot(posAst[:,0],posAst[:,1],posAst[:,2])
+#plt.plot(posAst[:,0],posAst[:,1],posAst[:,2])
+
 plt.show()
--- Planetary_Data.py
+++ Planetary_Data.py
@@ -5,9 +5,10 @@ names = ['Mercury',
          'Jupiter',
          'Moon',
          'Sun',
-         '2024YR4']#,
+         '2024YR4',
          #'Uranus',
-         #'Neptune']
+         #'Neptune'
+         ]
 
 masses = [3.285e23,
           4.867e24,
@@ -16,5 +17,7 @@ masses = [3.285e23,
           1.898e27,
           7.348e22,
           1.989e30,
-          1
+          1,
+          #8.681e25,
+          #1.024e26
           ]
